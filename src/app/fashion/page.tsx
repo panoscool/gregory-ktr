2b8544665530633cecd646fa81131c0078f512d8
Panos K <lkullolli@gmail.com>
2024-09-08 08:54:33 +0400

    Removes unnecessary margin

--- src/app/fashion/page.tsx
+++ src/app/fashion/page.tsx
@@ -12,33 +12,31 @@ export default function WorkPage() {
   const albums = getAlbums();
 
   return (
-    <div className="m-6">
-      <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full"
-      >
-        <CarouselContent className="-ml-1">
-          {albums.map((album) => (
-            <CarouselItem key={album.id} className="pl-0 md:basis-1/2 lg:basis-1/3">
-              <Image
-                src={album.src}
-                alt={album.name}
-                loading="lazy"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-                width={500}
-                height={300}
-              />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-opacity-50 bg-black p-2 rounded-full text-white" />
-        <CarouselNext className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-opacity-50 bg-black p-2 rounded-full text-white" />
-      </Carousel>
-    </div>
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full"
+    >
+      <CarouselContent className="-ml-1">
+        {albums.map((album) => (
+          <CarouselItem key={album.id} className="pl-0 md:basis-1/2 lg:basis-1/3">
+            <Image
+              src={album.src}
+              alt={album.name}
+              loading="lazy"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+              width={500}
+              height={300}
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-opacity-50 bg-black p-2 rounded-full text-white" />
+      <CarouselNext className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-opacity-50 bg-black p-2 rounded-full text-white" />
+    </Carousel>
   );
 }
